b0a40d6fa65d1e9c575a524279cd41226afa86ca
Netvoiangel <110711364+Netvoiangel@users.noreply.github.com>
2022-11-10 12:23:05 +0300

    Initial Commit

---
+++ data_base_1.0/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  data_base_1.0
+//
+//  Created by timur on 10.11.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
